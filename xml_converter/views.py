6f88eeed5da20f57db80087fe9ab991ff6d25ff3
Faruq <faruqabdulsalam@yahoo.com>
2022-03-16 09:48:38 +0100

    Chnage "none" keyword to empty string ("")

--- xml_converter/views.py
+++ xml_converter/views.py
@@ -39,8 +39,14 @@ def XML_to_json(file):
     # convert content of xml file to json
     json_data = json.dumps(data)
 
+
     # parse json_data from json string to python dictionary:
     converted = json.loads(json_data)
+
+    # change 'None' keyword to empty string "" 
+    for key in converted:
+        if converted[key] == None:
+            converted[key] = ""
 
     return converted
 
