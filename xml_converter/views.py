b76edef0e8ac0bf1013a7dd8972bc66dffb78f81
Faruq <faruqabdulsalam@yahoo.com>
2022-03-16 10:09:11 +0100

    Import XML conversion function from Utils.py

--- xml_converter/views.py
+++ xml_converter/views.py
@@ -4,10 +4,8 @@ from django.shortcuts import render
 # import XML upload form
 from .forms import Upload_XML_form
 
-# import json and xmltodict packages
-import json
-import xmltodict
-
+# import XML_to_json function from utils.py file
+from .utils import XML_to_json
 
 def upload_page(request):
 
@@ -26,28 +24,3 @@ def upload_page(request):
         'form' : form,
         }
     return render(request, "upload_page.html", context)
-
-
-def XML_to_json(file):
-    # open xml file
-    with file as xml_file:
-        data = xmltodict.parse(xml_file.read())
-
-    # close xml file after parsing the content
-    xml_file.close()
-
-    # convert content of xml file to json
-    json_data = json.dumps(data)
-
-
-    # parse json_data from json string to python dictionary:
-    converted = json.loads(json_data)
-
-    # change 'None' keyword to empty string "" 
-    for key in converted:
-        if converted[key] == None:
-            converted[key] = ""
-
-    return converted
-
-
