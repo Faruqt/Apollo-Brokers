a03b0a1b3d9edd33fd011af9cd1f8abb1d4c9aae
Faruq <faruqabdulsalam@yahoo.com>
2022-03-16 11:24:57 +0100

    Update tests.py file

--- xml_converter/tests.py
+++ xml_converter/tests.py
@@ -22,9 +22,10 @@ class XMLConversionTestCase(TestCase):
 
     def test_api_convert_empty_document(self):
         with (TEST_DIR / Path('empty.xml')).open() as fp:
+            # change key from 'file' to 'files'
             response = self.client.post('/api/converter/convert/', {
-                'file': fp,
-            })
+                'files': fp}
+                , format='multipart')
             self.assertEqual(response.status_code, 200)
             self.assertEqual(response.json(), {
                 "Root": "",
@@ -37,23 +38,59 @@ class XMLConversionTestCase(TestCase):
             })
             self.assertEqual(response.status_code, 200)
             self.assertEqual(response.json(), {
-                "Root": [
+                #adjust only the address to be an array since it occurs twice
+                "Root":
                     {
                         "Address": [
-                            {"StreetLine1": "123 Main St."},
-                            {"StreetLine2": "Suite 400"},
-                            {"City": "San Francisco"},
-                            {"State": "CA"},
-                            {"PostCode": "94103"},
+                            {"StreetLine1": "123 Main St.",
+                            "StreetLine2": "Suite 400",
+                            "City": "San Francisco",
+                            "State": "CA",
+                            "PostCode": "94103"}
+                            ,
+                            {"StreetLine1": "400 Market St.",
+                            "City": "San Francisco",
+                            "State": "CA",
+                            "PostCode": "94108"}
                         ]
                     },
-                    {
-                        "Address": [
-                            {"StreetLine1": "400 Market St."},
-                            {"City": "San Francisco"},
-                            {"State": "CA"},
-                            {"PostCode": "94108"},
-                        ]
+            })
+
+    def test_connected_convert_schools(self):
+        with (TEST_DIR / Path('schools.xml')).open() as fp:
+            response = self.client.post('/connected/', {
+                'file': fp,
+            })
+            self.assertEqual(response.status_code, 200)
+            self.assertEqual(response.json(), {
+                "Root": {
+                    "Harvard": {
+                        "Alumnus": [
+                            "John F. Kennedy",
+                            "Mark Zuckerberg",
+                            "Barack Obama",
+                            "Conan O'Brien"
+                        ],
+                        "Departments": [
+                            "Engineering",
+                            "Mathematics",
+                            "History"
+                        ],
+                        "Location": "Cambridge, Massachusetts"
                     },
-                ],
+                    "MIT": {
+                        "Alumnus": [
+                            "Buzz Aldrin",
+                            "Ilene S. Gordon",
+                            "Richard Feynman",
+                            "Jonah Peretti"
+                        ],
+                        "Departments": [
+                            "Engineering",
+                            "Mathematics",
+                            "History"
+                        ],
+                        "Location": "Cambridge, Massachusetts"
+                    }
+                }
             })
