548dc90638795e9b5ef02882f03164aabec7dbae
Faruq <faruqabdulsalam@yahoo.com>
2022-03-16 09:07:06 +0100

    Create XML_to_json custom function

--- xml_converter/views.py
+++ xml_converter/views.py
@@ -1,19 +1,47 @@
 from django.http import JsonResponse
 from django.shortcuts import render
-from .forms import UploadXMLform
+
+# import XML upload form
+from .forms import Upload_XML_form
+
+# import json and xmltodict packages
+import json
+import xmltodict
 
 
 def upload_page(request):
-    
-    form = UploadXMLform()
+
+    # TODO: Convert the submitted XML file into a JSON object and return to the user.
+
+    form = Upload_XML_form()
     if request.method == 'POST':
-        form = UploadXMLform(request.POST, request.FILES)
+        form = Upload_XML_form(request.POST, request.FILES)
         if form.is_valid():
-            handle_uploaded_file(request.FILES['file'])
+            converted_XML = XML_to_json(request.FILES['file'])
         else:
             form = UploadXMLform()
-        # TODO: Convert the submitted XML file into a JSON object and return to the user.
-        return JsonResponse({})
+        return JsonResponse(converted_XML)
 
-    context = {'form' : form}
+    context = {
+        'form' : form,
+        }
     return render(request, "upload_page.html", context)
+
+
+def XML_to_json(file):
+    # open xml file
+    with file as xml_file:
+        data = xmltodict.parse(xml_file.read())
+
+    # close xml file after parsing the content
+    xml_file.close()
+
+    # convert content of xml file to json
+    json_data = json.dumps(data)
+
+    # parse json_data from json string to python dictionary:
+    converted = json.loads(json_data)
+
+    return converted
+
+
