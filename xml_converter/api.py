bf17625f014ce5e48cf4fe43c16b1a5a9044d2dc
Faruq <faruqabdulsalam@yahoo.com>
2022-03-16 10:08:13 +0100

    Import and add serializer form to viewset

--- xml_converter/api.py
+++ xml_converter/api.py
@@ -3,8 +3,15 @@ from rest_framework.parsers import MultiPartParser
 from rest_framework.response import Response
 from rest_framework.viewsets import ViewSet
 
+# import XMLform serializer 
+from .serializers import XMLSerializer
+
+# import XML_to_json function from utils.py file
+from .utils import XML_to_json
 
 class ConverterViewSet(ViewSet):
+
+    serializer_class = XMLSerializer
     # Note this is not a restful API
     # We still use DRF to assess how well you know the framework
     parser_classes = [MultiPartParser]
